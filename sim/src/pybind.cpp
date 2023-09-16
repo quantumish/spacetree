73047ad608e7abfd3b9fa55b697f164b7916460f
quantumish <freifeld.david@gmail.com>
2023-09-16 01:59:46 -0400

    Set up pybind11

---
+++ sim/src/pybind.cpp
@@ -0,0 +1,24 @@
+#include "bhut.hpp"
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/functional.h>
+#include <pybind11/eigen.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(nbody_dev, m) {
+	m.doc() = "Debugging utilities for nbody.";
+	py::class_<Body>(m, "Body")
+		.def(py::init<float, Eigen::Vector3d, Eigen::Vector3d, Eigen::Vector3d>())
+		.def_readonly("mass", &Body::mass)
+		.def_readonly("position", &Body::p)
+		.def_readonly("velocity", &Body::v)
+		.def_readonly("acceleration", &Body::a);
+	py::class_<Node>(m, "Node")
+		.def_readonly("min", &Node::min)
+		.def_readonly("max", &Node::max)
+		.def_readonly("cm", &Node::cm)
+		.def_readonly("body", &Node::body)
+		.def_static("build_octree", &Node::build_octree);
+}

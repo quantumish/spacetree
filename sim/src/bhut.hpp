c3325c95acdc80e663741970ee89704e2922f38f
quantumish <freifeld.david@gmail.com>
2023-09-15 21:04:51 -0400

    Add initial stab at class definitions for Node and Body (and CMakeLists)

---
+++ sim/src/bhut.hpp
@@ -0,0 +1,22 @@
+#include <array>
+
+#include <Eigen/Dense>
+
+class Body {
+    float mass;
+    Eigen::Vector3d p;
+    Eigen::Vector3d v;
+    Eigen::Vector3d a;
+
+    Body();
+    Body(float m, Eigen::Vector3d pos, Eigen::Vector3d vel, Eigen::Vector3d accel);
+};
+
+class Node {
+    Body body;
+    Eigen::Vector3d com;
+    std::array<Node*, 8> children;
+
+    Node();
+};
+
